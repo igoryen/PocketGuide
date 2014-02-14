c0d27c72f7c1e9cf9658d7f1b15496862d403b19
igoryen <igor.yentaltsev@gmail.com>
2014-02-14 12:25:36 -0500

    Changes that broke the app 140214f 12:20

--- src/com/igoryen/pocketguide/SecondActivity.java
+++ src/com/igoryen/pocketguide/SecondActivity.java
@@ -2,20 +2,22 @@ package com.igoryen.pocketguide;
 
 //import android.app.ActionBar;
 import android.app.Activity;
-//import android.app.ListActivity;
+import android.app.ListActivity;
 //import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 import android.widget.Toast;
-//import android.widget.ListView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;;
 
 
-public class SecondActivity extends Activity{
+public class SecondActivity extends ListActivity{
 	
 	String[] terminology;
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +28,29 @@ public class SecondActivity extends Activity{
 		
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_dropdown_item_1line, terminology);
+	    
+		AutoCompleteTextView textView = (AutoCompleteTextView)findViewById(R.id.terms);
 
-	    AutoCompleteTextView textView = (AutoCompleteTextView)findViewById(R.id.terms);
+        /*
+		//---------------------------------------------------
+		//setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, terminology));
+		setListAdapter(adapter);
+		ListView list = getListView();
+		list.setTextFilterEnabled(true);
+		list.setOnItemClickListener(new OnItemClickListener(){ 
 
-	        textView.setThreshold(3);
-	        textView.setAdapter(adapter);
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				Toast.makeText(getApplicationContext(), ((TextView)arg1).getText(), Toast.LENGTH_LONG).show();
+			}
+			
+		});
+		//-------------------------------------------------
+		 * 
+		 */
+        textView.setThreshold(3);
+        textView.setAdapter(adapter);
+
 	}
 	
 	private void DisplayToast(String msg){
